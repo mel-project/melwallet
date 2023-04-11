a6a5260b917127563512b5e3f60210ce5a2c4f01
thisbefruit <thisbefruit@protonmail.com>
2023-04-11 12:14:12 -0500

    fixed confusing semantics

--- src/lib.rs
+++ src/lib.rs
@@ -80,7 +80,7 @@ impl Wallet {
         for k in spent_coins {
             self.confirmed_utxos.remove(&k);
         }
-        self.height += BlockHeight(1);
+        self.height = height;
         Ok(())
     }
 
